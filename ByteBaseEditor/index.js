dde745077227c04b44d1a922f83c02b3613eb26d
Prithviraj Nalawade <75968727+prithviraj-nalawade@users.noreply.github.com>
2025-02-21 10:54:11 +0530

    Update index.js

--- ByteBaseEditor/index.js
+++ ByteBaseEditor/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import session from "express-session";
 import nodemailer from "nodemailer";
 import fs from "fs";
-import { connectDB, getConnection } from "./DButils/dbutil.js";
+//import { connectDB, getConnection } from "./DButils/dbutil.js";
 import { exec } from "child_process";
 
 const app = express();
@@ -31,159 +31,6 @@ app.use(session({
 //         pass: "fkuw zykc jmop oejz"
 //     }
 // })
-
-let email1 = "";
-
-
-const connection = connectDB();
-
-
-app.post("/", (req, res) => {
-    try {
-        const { email } = req.body;
-        // console.log(req);
-
-        if (!email) {
-            return res.status(400).send({ msg: "Email is required" });
-        }
-
-        const otp = Math.floor(1000 + Math.random() * 9000);
-        req.session.otp = otp;
-        req.session.email = email;
-        email1 = email;
-        // console.log(req.session);
-
-        console.log(`Generated OTP for ${email}: ${otp}`);
-
-
-        connection.query(`SELECT * FROM students WHERE email='${email}'`, (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send({ msg: "Database error" });
-            }
-
-            if (result.length > 0) {
-                // await transport.sendMail({
-                //     to: `${result[0].email}`,
-                //     subject: "OTP Verification",
-                //     html: `<h2>OTP verification: ${otp}</h2>`
-                // })
-                if (result[0].password === null) {
-
-                    // res.redirect("/otpverify");
-                    return res.status(200).send({ msg: "OTP generated successfully" });
-                } else {
-                    return res.status(201).send({ msg: "Already has an password" });
-                }
-            } else {
-                return res.status(204).send({ msg: "Email not found" });
-            }
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ msg: "Internal server error" });
-    }
-});
-
-
-app.post("/verify", (req, res) => {
-    try {
-        const { otp } = req.body;
-
-        if (!otp) {
-            return res.status(203).send({ msg: "OTP is required" });
-        }
-
-        console.log(req.session.email);
-        if (parseInt(otp) === 1234) {
-
-            // Clear the OTP after successful verification
-            req.session.otp = null;
-            return res.status(200).send({ msg: "OTP verified successfully" });
-        } else {
-            return res.status(204).send({ msg: "Invalid or expired OTP" });
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ msg: "Internal server error" });
-    }
-});
-
-app.post("/addpassword", (req, res) => {
-    try {
-        const { password } = req.body;
-        // console.log(req);
-
-        if (!password) {
-            return res.status(400).send({ msg: "Password is required" });
-        }
-
-
-        console.log(email1, password);
-
-        connection.query(`update students set password='${password}' where email='${email1}'`, (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send({ msg: "Database error" });
-            }
-
-
-            // await transport.sendMail({
-            //     to: `${result[0].email}`,
-            //     subject: "OTP Verification",
-            //     html: `<h2>OTP verification: ${otp}</h2>`
-            // })
-
-
-            // res.redirect("/otpverify");
-            const data1 = result[0];
-            return res.status(200).send({ msg: "Password Updated", data1 });
-
-
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ msg: "Internal server error" });
-    }
-});
-
-app.post("/verifypassword", (req, res) => {
-    try {
-        const { password } = req.body;
-        // console.log(req);
-
-        if (!password) {
-            return res.status(400).send({ msg: "Email is required" });
-        }
-
-
-
-        console.log(email1, password);
-        connection.query(`SELECT * FROM students WHERE email='${email1}' and password='${password}'`, (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send({ msg: "Database error" });
-            }
-
-            if (result.length > 0) {
-                // await transport.sendMail({
-                //     to: `${result[0].email}`,
-                //     subject: "OTP Verification",
-                //     html: `<h2>OTP verification: ${otp}</h2>`
-                // })
-                console.log(result[0]);
-                const data1 = result[0];
-                res.status(200).send({ msg: "Login Success", data1 });
-
-            } else {
-                return res.status(204).send({ msg: "Email not found" });
-            }
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ msg: "Internal server error" });
-    }
-});
 
 
 
@@ -232,5 +79,5 @@ app.post("/editor", (req, res) => {
 
 app.listen(9800, () => {
     console.log("Server is running on port 9800...");
-    getConnection();
+   
 });
